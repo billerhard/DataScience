36d1a6a491c5c315832733970673f4b7d8aa859b
Bill <wherhard@student.rtc.edu>
2018-11-08 21:23:02 -0800

    initial work

---
+++ 10minutepandas.py
@@ -0,0 +1,10 @@
+# Learning pandas in about 10 minutes
+# https://pandas.pydata.org/pandas-docs/stable/10min.html
+# CNA 330
+# Bill Erhard, wherhard@student.rtc.edu
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+s = pd.Series([1, 3, 5, np.nan, 6, 8])
