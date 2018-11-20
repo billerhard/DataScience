727b270f30ae84f25bf3f97b990133882764679c
Bill <wherhard@student.rtc.edu>
2018-11-20 10:54:33 -0800

    Prettily and safely fetch world happiness report 2018, store in xls.

--- worldhappinessreport.py
+++ worldhappinessreport.py
@@ -2,9 +2,12 @@
 # CNA 330
 # Bill Erhard, wherhard@student.rtc.edu
 import requests
+from pathlib import Path
+import pandas
+import xlrd
 
 
-# returns xls as binary from website
+# Fetches 2018 world happiness report from aws s3 in xls binary format
 def fetch_xls():
 
     query = "https://s3.amazonaws.com/happiness-report/2018" \
@@ -15,8 +18,23 @@ def fetch_xls():
     return binary
 
 
+# Uses fetch_xls to get 2018 world happiness report and stores it in
+# "world_happiness.xls" (only need to run once)
+def get_and_store_report(path):
+
+    with open(path, "wb") as f:
+        f.write(fetch_xls())
+
+
 def main():
-    fetch_xls()
+
+    # If we don't have the report, get it.
+    world_happiness_file = Path("./world_happiness.xls")
+    if not world_happiness_file.exists():
+        get_and_store_report(world_happiness_file)
+
+    with open(world_happiness_file, "r") as f:
+        print(f)
 
 
 if __name__ == '__main__':
