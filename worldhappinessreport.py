744a1668f908c50b53156ce0c5b513b800c2e9a5
Bill <wherhard@student.rtc.edu>
2018-11-20 17:21:27 -0800

    cleanup, start work on db

--- worldhappinessreport.py
+++ worldhappinessreport.py
@@ -1,13 +1,11 @@
 # Learning world happiness
 # CNA 330
 # Bill Erhard, wherhard@student.rtc.edu
-from typing import List, Any
 
-import requests
 from pathlib import Path
+from matplotlib import pyplot
 import pandas
-import numpy
-import matplotlib.pyplot
+import requests
 
 
 # Fetches 2018 world happiness report from aws s3 in xls binary format
@@ -29,7 +27,7 @@ def get_and_store_report(path):
         f.write(fetch_xls())
 
 
-# Open and read the excel spreadsheet (specifically Figure 2.3, index *2*)
+# Open and read the excel spreadsheet, specifically Figure 2.3
 def print_figure(world_happiness_file, sheet):
 
     data_frame = pandas.read_excel(world_happiness_file, sheet)
@@ -38,19 +36,28 @@ def print_figure(world_happiness_file, sheet):
 
 # Make a bar graph from Figure 2.2, just Country vs. Happiness
 def plot_bar_graph(world_happiness_file, sheet):
+
     data_frame = pandas.read_excel(world_happiness_file, sheet)
+
     # Sheet has a lot of bloat, trim it down to countries and scores.
     data_frame = data_frame.iloc[:-1, 0:2]
+
     # Set index for plot
     data_frame.index = data_frame['Country']
+
     # Ready to plot and show!
     data_frame.plot.bar()
-    matplotlib.pyplot.show()
+    pyplot.show()
+
+
+def create_database(database):
+    pass
 
 
 def main():
-    # Path to file
+    # Setup Files (xls and sql)
     world_happiness_file = Path("./world_happiness.xls")
+    database = Path("./world_happiness.sql")
 
     # If we don't have the report, get it into an xls.
     if not world_happiness_file.exists():
@@ -59,12 +66,11 @@ def main():
     # Print Figure 2.3
     print_figure(world_happiness_file, 2)
 
-    # Make a bar graph
+    # Make a bar graph from Figure 2.2
     plot_bar_graph(world_happiness_file, 1)
 
     # Create SQL Database
-
-
+    create_database(database)
 
 
 if __name__ == '__main__':
